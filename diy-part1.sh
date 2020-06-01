69258f1408e74667603ba50492dc4890e314c807
朱晓军 <1365441936@qq.com>
2020-06-01 16:47:20 +0800

    custom

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,12 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+sed -i 's/192.168.1.1/10.11.12.1/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
