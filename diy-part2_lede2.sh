066ccca071e9088e43d449a9f986512f9b76378c
朱晓军 <1365441936@qq.com>
2020-06-10 16:02:53 +0800

    添加lede2

---
+++ diy-part2_lede2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+sed -i 's/192.168.1.1/10.11.12.252/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
